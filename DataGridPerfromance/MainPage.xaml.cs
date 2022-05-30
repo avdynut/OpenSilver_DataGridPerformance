8f6eb67d9c9a564b82f693f858013b0b0fcb865a
Andrey Arekhva <a.arekhva@gmail.com>
2022-05-30 14:17:58 +0300

    Add collection wrapper

--- DataGridPerfromance/MainPage.xaml.cs
+++ DataGridPerfromance/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System;
+using System.Collections;
 #if OPENSILVER
 using System.Diagnostics;
 #endif
@@ -13,7 +14,7 @@ namespace DataGridPerfromance
 {
     public partial class MainPage : UserControl
     {
-        private ObservableCollection<Item> _items = new ObservableCollection<Item>();
+        private List<Item> _items = new List<Item>();
         private ObservableCollection<Item> _items2 = new ObservableCollection<Item>();
         private ObservableCollection<Item> _items3 = new ObservableCollection<Item>();
         private ICollectionView _collectionView;
@@ -23,18 +24,19 @@ namespace DataGridPerfromance
         {
             InitializeComponent();
 
-            MyDataGrid.ItemsSource = _items;
+            
 
             _collectionView = new CollectionViewSource { Source = _items2 }.View;
             // _collectionView.SortDescriptions.Add(new SortDescription("Number", ListSortDirection.Ascending));
 
-            _collectionView2 = new CollectionViewSource { Source = new Collection<Item>(_items3) }.View;
+            _collectionView2 = new CollectionViewSource { Source = new CollectionWrapper<Item>(_items3) }.View;
             CollectionViewListDataGrid.ItemsSource = _collectionView2;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             RefreshCollection(_items, ListTime);
+            MyDataGrid.ItemsSource = new CollectionViewSource { Source = _items }.View;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -66,5 +68,25 @@ namespace DataGridPerfromance
                 textBlock.Text = $"{stopwatch.ElapsedMilliseconds} ms";
             }
         }
+    }
+
+    public class CollectionWrapper<T> : ICollection<T>
+    {
+        private readonly ICollection<T> _items;
+
+        public CollectionWrapper(ICollection<T> items)
+        {
+            _items = items ?? throw new ArgumentNullException(nameof(items));
+        }
+
+        public int Count => _items.Count;
+        public bool IsReadOnly => _items.IsReadOnly;
+        public void Add(T item) => _items.Add(item);
+        public bool Remove(T item) => _items.Remove(item);
+        public bool Contains(T item) => _items.Contains(item);
+        public void Clear() => _items.Clear();
+        public void CopyTo(T[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_items).GetEnumerator();
     }
 }
