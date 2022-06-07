127603e2324c90aada9461ac5db7e627d0d8f623
Andrey Arekhva <a.arekhva@gmail.com>
2022-06-07 10:58:44 +0300

    Add test datagrid to detach/attach collection

--- DataGridPerfromance/MainPage.xaml.cs
+++ DataGridPerfromance/MainPage.xaml.cs
@@ -15,9 +15,11 @@ namespace DataGridPerfromance
     {
         private ObservableCollection<Item> _items = new ObservableCollection<Item>();
         private ObservableCollection<Item> _items2 = new ObservableCollection<Item>();
-        private List<Item> _items3 = new List<Item>();
+        private ObservableCollection<Item> _items3 = new ObservableCollection<Item>();
+        private ObservableCollection<Item> _items4 = new ObservableCollection<Item>();
         private ICollectionView _collectionView;
         private ICollectionView _collectionView2;
+        private ICollectionView _collectionView3;
 
         public MainPage()
         {
@@ -26,11 +28,13 @@ namespace DataGridPerfromance
             MyDataGrid.ItemsSource = _items;
 
             _collectionView = new CollectionViewSource { Source = _items2 }.View;
-            // _collectionView.SortDescriptions.Add(new SortDescription("Number", ListSortDirection.Ascending));
+            //_collectionView.SortDescriptions.Add(new SortDescription("Number", ListSortDirection.Ascending));
             CollectionViewDataGrid.ItemsSource = _collectionView;
 
-            _collectionView2 = new CollectionViewSource { Source = _items3 }.View;
+            _collectionView2 = new CollectionViewSource { Source = new Collection<Item>(_items3) }.View;
             CollectionViewListDataGrid.ItemsSource = _collectionView2;
+
+            _collectionView3 = new CollectionViewSource { Source = _items4 }.View;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -46,6 +50,12 @@ namespace DataGridPerfromance
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             RefreshCollection(_items3, CollectionViewListTime, _collectionView2.Refresh);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            CollectionViewAttachDetachDataGrid.ItemsSource = null;
+            RefreshCollection(_items4, CollectionViewAttachDetachTime, () => CollectionViewAttachDetachDataGrid.ItemsSource = _collectionView3);
         }
 
         private void RefreshCollection(IList<Item> list, TextBlock textBlock, Action action = null)
@@ -64,6 +74,14 @@ namespace DataGridPerfromance
                 action?.Invoke();
                 stopwatch.Stop();
                 textBlock.Text = $"{stopwatch.ElapsedMilliseconds} ms";
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is DataGrid dataGrid && dataGrid.ItemsSource is ICollectionView view)
+            {
+                view.Refresh();
             }
         }
     }
